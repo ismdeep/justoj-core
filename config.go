b2d4a06f38cc826b995cd4b2f22c8a5c42d2ca68
L. Jiang <l.jiang.1024@gmail.com>
2021-08-18 10:04:26 +0800

    update

--- config.go
+++ config.go
@@ -1,4 +1,4 @@
-package utils
+package main
 
 import (
 	"encoding/json"
@@ -19,10 +19,6 @@ type Config struct {
 	SleepTime       int        `json:"sleep_time"`        // 服务器无用户提交后程序休眠时间
 	ClientName      string     `json:"client_name"`       // 客户端名称
 	CpuCompensation float64    `json:"cpu_compensation"`  // CPU性能放大倍数
-	JavaTimeBonus   float64    `json:"java_time_bonus"`   // Java程序运行时间红利倍数
-	JavaMemoryBonus float64    `json:"java_memory_bonus"` // Java程序运行内容红利倍数
-	JavaXms         string     `json:"java_xms"`          // Java Xms 参数
-	JavaXmx         string     `json:"java_xmx"`          // Java Xmx 参数
 	DataPath        string     `json:"data"`              // 数据存放地址
 	ClientExec      string     `json:"client"`            // justoj-core-client 绝对路径
 	SecureCode      string     `json:"secure_code"`       // 服务器用于判题机认证的Secure Code，存放在 justoj 项目的 .env 文件中
--- main.go
+++ main.go
@@ -2,12 +2,11 @@ package main
 
 import (
 	"fmt"
-	"github.com/ismdeep/justoj-core/utils"
 	"log"
 )
 
 func main() {
-	config, err := utils.LoadConfig("config.example.json")
+	config, err := LoadConfig("config.example.json")
 	if err != nil {
 		log.Fatalln(err)
 		return
